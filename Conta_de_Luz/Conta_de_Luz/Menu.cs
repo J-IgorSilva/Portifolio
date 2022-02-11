5e90b6a4b3b5688975179bdad576796519c47f9b
J-IgorSilva <aleatoria8821@gmail.com>
2022-02-10 23:43:22 -0300

    Mudar Metodos de staticos para publicos

--- Conta_de_Luz/Conta_de_Luz/Menu.cs
+++ Conta_de_Luz/Conta_de_Luz/Menu.cs
@@ -4,27 +4,60 @@ namespace Conta_de_Luz
 {
     class Menu
     {
-        public void menuu(string opcao) // criar um mentoodo ou função para as opções
+        public void Menuu() // criar um mentoodo ou função para as opções
         {
-            Console.Clear();
+            Console.WriteLine("Ola Seja Bem vindo, Esolha uma das Opções Abaixo! ");
+
+            Console.WriteLine("------------------------------");
 
             Console.WriteLine("1 Cadastro");
             Console.WriteLine("2 Calculo da Conta de Luz");
-            Console.WriteLine("3 Calculo da Conta de Agua");
+           
             Console.WriteLine("0 Sair");
 
-            switch (opcao)
+            short escolha = short.Parse(Console.ReadLine());
+
+            switch (escolha)
             {
-                case "1":
-                       Console.WriteLine// função para cadastro
-                       Console.WriteLine//calculo
-                        Console.WriteLine//agua
-                        Console.WriteLine//sair
-               
-                case 0: System.Environment.Exit(0); break;
-                default: opcao(); break;
+                case 1: InsertName();
+                    break;
+                case 2: InsertConta();
+                    break;
+                case 0: System.Environment.Exit(0);
+                    break;
+
+            }
+            static void InsertName()
+            {
+                Console.Clear();
+                Console.WriteLine("Ola, Vamos Iniciar sua consulta a Seguir ");
+
+                Console.WriteLine();
+
+                Console.WriteLine("Insira seu Nome e Sobrenome");
+
+                var name = Console.ReadLine();
+
+
+            }
+            public void InsertConta()
+            {
+                Console.Clear();
+
+                Console.WriteLine("Insira o Valor do Consumo, Sem os Impostos");
+
+                float ligth = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite Quantos Kw/H  Você Consumiu");
+
+                float kwh = float.Parse(Console.ReadLine());
+
+                float total = ligth / kwh;
+
+                Console.WriteLine($"O Valor Pago Por KH/W é de ${total} Sem Impostos.");
+
             }
         }
-    }
+       
     }
 }
--- Conta_de_Luz/Conta_de_Luz/Program.cs
+++ Conta_de_Luz/Conta_de_Luz/Program.cs
@@ -6,68 +6,16 @@ using System.Threading.Tasks;
 
 namespace Conta_de_Luz
 {
-    
-    class Program : Dadospessoa
+
+    class Program
     {
 
         static void Main(string[] args)
         {
             Menu m = new Menu();
+            m.Menuu();
             
-
-            string name, mess;
-            float luzz, khww;
-            Dadospessoa D = new Dadospessoa();
-            Calculo_Conta C = new Calculo_Conta();
-
-            Console.WriteLine("Ola, Insira seu Nome Para começar: ");
-            name = Console.ReadLine();
-            
-            Console.WriteLine("--------------------------");
-
-            Console.WriteLine($" Ola {name} Seja Bem Vindo! ");
-
-            Console.WriteLine("Escolha Uma Opção Abaixo: ");
-            Console.WriteLine();
-            m.menuu();
-
-            string[] escolha = new string[12];
-            {
-                escolha[0] = "1 - Janeiro";
-                escolha[1] = "2 - Fevereiro";
-                escolha[2] = "3 - Março";
-                escolha[3] = "4 - Abril";
-                escolha[4] = "5 - Maio";
-                escolha[5] = "6 - Junho";
-                escolha[6] = "7 - Julho";
-                escolha[7] = "8 - Agosto";
-                escolha[8] = "9 - Setembro";
-                escolha[9] = "10 - Outubro";
-                escolha[10] ="11 - Novembro";
-                escolha[11] = "12 - Dezembro";
-                for(int i = 0; i < 11; i++)
-                {
-                    Console.WriteLine(escolha[i]);
-                }
-            }
-        
-            
-
-            Console.WriteLine("--------------------------");
-
-            D.Nome(name);
-
-            Console.WriteLine("Agora insira qual o valor total da sua Fatura, e Quantos Kw/H De consumo no mes: ");
-
-            luzz = float.Parse(Console.ReadLine());
-            khww = float.Parse(Console.ReadLine());
-
-
-            C.Calculo(luzz, khww);
-
-            Console.WriteLine(C.total);
-            Console.ReadKey();
-          
         }
     }
+
 }
