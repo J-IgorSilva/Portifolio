df674b6a4192544c8f1eb777d98372d54107a431
J-IgorSilva <aleatoria8821@gmail.com>
2022-11-10 13:03:17 -0300

    Ajustes

--- ControleDeContatos/ControleDeContatos/Repositorio/ContatoRepositorio.cs
+++ ControleDeContatos/ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -12,14 +12,17 @@ namespace ControleDeContatos.Repositorio
         {
             _bancoContext = bancoContext;
         }
+
         public List<ContatoModel> ListarTodosContatos()
         {
             return _bancoContext.Contatos.ToList();
         }
+
         public ContatoModel EditarPorId(int id)
         {
             return _bancoContext.Contatos.FirstOrDefault(TodosContatos => TodosContatos.Id == id);
         }
+        
         public ContatoModel Adicionar(ContatoModel contato)
         {
             _bancoContext.Contatos.Add(contato);
