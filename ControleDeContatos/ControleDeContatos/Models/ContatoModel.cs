63684f04c34f919476b2b76966c0e018aeb6e93c
J-IgorSilva <aleatoria8821@gmail.com>
2022-11-11 00:24:08 -0300

    Required de campos form

--- ControleDeContatos/ControleDeContatos/Models/ContatoModel.cs
+++ ControleDeContatos/ControleDeContatos/Models/ContatoModel.cs
@@ -1,10 +1,17 @@
-﻿namespace ControleDeContatos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models
 {
     public class ContatoModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Insira o Nome do contato")]
         public string Nome  { get; set; }
+        [Required(ErrorMessage = "Insira o Email do contato")]
+        [EmailAddress(ErrorMessage = "Email invalido")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Insira o Celular do contato")]
+        [Phone(ErrorMessage = "Telefone invalido")]
         public string Celular { get; set; }
     }
 }
