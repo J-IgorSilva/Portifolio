e7342d452b59e949ca5119329dcb5b2f74e4dbeb
J-IgorSilva <aleatoria8821@gmail.com>
2022-11-29 08:25:42 -0300

    Criando ApiEntregador

---
+++ EntregaAgua/EntregaAgua/Models/Contexto.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntregaAgua.Models
+{
+    public class Contexto : DbContext
+    {
+
+    }
+}
---
+++ EntregaAgua/EntregaAgua/Models/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntregaAgua.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name="Codigo")]
+        public int Id { get; set; }
+
+        [Column("Agua")]
+        [Display(Name = "Pedido")]
+        public int Agua { get; set; }
+
+        [Column("Entregador")]
+        [Display(Name = "Entregador")]
+        public int Entregador { get; set; }
+
+        [Column("Cliente")]
+        [Display(Name = "Cliente")]
+        public string Cliente { get; set; }
+    }
+
+}
