4f8b47752ad32e4cac50a546b589745cde73a164
J-IgorSilva <aleatoria8821@gmail.com>
2023-02-01 07:54:21 -0300

    Implementando endpoit Delete em Categorias

--- MiniApiCatalogo/MiniApiCatalogo/Context/AppDbContext.cs
+++ MiniApiCatalogo/MiniApiCatalogo/Context/AppDbContext.cs
@@ -8,7 +8,7 @@ namespace MiniApiCatalogo.Context
         public AppDbContext(DbContextOptions<AppDbContext>options):base (options)
         {
         }
-        public DbSet<Produto>? Produtos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
